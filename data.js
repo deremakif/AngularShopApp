f0c50732ea74f3845bee4640da54fed6eaddd870
Akif <akif.dere@rlcteknoloji.com>
2019-08-14 21:00:41 +0300

    106 - Proje yapısının hazırlanması.

---
+++ data.js
@@ -0,0 +1,12 @@
+module.exports = function() {
+    return {
+        products: [
+            { id:1, name:"Samsung S5", price:1000, imageUrl:'1.jpg', description:'idare eder.'}
+
+        ],
+        categories: [
+
+        ],
+        orders: []
+    }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'root',
+  template: `<div class="bg-primary m-5 p-5">
+                Shop Application
+             </div>`
+  
 })
 export class AppComponent {
-  title = 'ShopApp';
+
 }
