c50429ca047baeefefa233664f788235924760ca
Akif <akif.dere@rlcteknoloji.com>
2019-08-14 21:43:32 +0300

    108 - Http Metodlarının Hazırlanması

---
+++ src/app/model/category.model.ts
@@ -0,0 +1,8 @@
+export class Category {
+    constructor (
+        public id?: number,
+        public name?: string
+       
+
+    ) {}
+}
--- src/app/model/rest.service.ts
+++ src/app/model/rest.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Product } from './product.model';
+import { Category } from './category.model';
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +13,14 @@ export class RestService {
 
   constructor(private http: HttpClient) { }
 
-  getProducts() {
-    return this.http.get(this.baseUrl + 'products');
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.baseUrl + 'products');
   }
+
+  getCategories(): Observable<Category[]> {
+    return this.http.get<Category[]>(this.baseUrl + 'categories');
+  }
+
+  // getProduct.subscribe()
 
 }
