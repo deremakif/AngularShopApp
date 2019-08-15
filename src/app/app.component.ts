0a4bdee8d17b64664f3c6c5167d2026d192c9dd8
Akif <akif.dere@rlcteknoloji.com>
2019-08-15 09:54:38 +0300

    backtick(``) yerine quote('') kullanıldı.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'root',
-  template: `<shop></shop>`
+  template: "<shop></shop>"
   
 })
-export class AppComponent {
 
+export class AppComponent {
 }
