cad272acef0cecbdfe91ae47a1392f9c5a5a1669
Akif <akif.dere@rlcteknoloji.com>
2019-08-16 09:29:21 +0300

    115 - Sayfalama

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -15,13 +15,18 @@ import { Category } from '../model/category.model';
 export class ShopComponent 
 {
     public selectedCategory:Category = null;
+    public productsPerPage = 3;
+    public selectedPage = 1;
 
     constructor(
         private productRepository: ProductRepository,
         private categoryRepository: CategoryRepository) {}
 
     get products(): Product[] {
-        return this.productRepository.getProducts(this.selectedCategory);
+        let index = (this.selectedPage - 1) * this.productsPerPage; 
+        return this.productRepository
+                .getProducts(this.selectedCategory)
+                .slice(index, index + this.productsPerPage);
     }
 
     get categories(): Category[] {
