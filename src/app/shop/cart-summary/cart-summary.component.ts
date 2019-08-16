1a3d1385fb74863cd6f8d0b31181ed29f0541131
Akif <akif.dere@rlcteknoloji.com>
2019-08-16 21:16:33 +0300

    13. Sepete Ekle Butonu ve Kart Özeti

--- src/app/shop/cart-summary/cart-summary.component.ts
+++ src/app/shop/cart-summary/cart-summary.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Cart } from 'src/app/model/cart.model';
 
 @Component({
   selector: 'cart-summary',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartSummaryComponent implements OnInit {
 
-  constructor() { }
+  constructor(public cart: Cart) { }
 
   ngOnInit() {
   }
