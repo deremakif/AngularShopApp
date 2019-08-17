d17e14eb04c0231c2623e7d30b39ec104ab6c50f
Akif <akif.dere@rlcteknoloji.com>
2019-08-17 18:57:16 +0300

    26. Authentication Guard

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
 import { AdminComponent } from './admin.component';
+import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
   {path: 'auth', component: AuthComponent },
-  {path: 'main', component: AdminComponent },
+  {path: 'main', component: AdminComponent, canActivate:[AuthGuard] },
   {path: '**', redirectTo: 'auth' }
 ];
 
--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -5,6 +5,7 @@ import { AdminRoutingModule } from './admin-routing.module';
 import { AuthComponent } from './auth/auth.component';
 import { AdminComponent } from './admin.component';
 import { FormsModule } from '@angular/forms';
+import { AuthGuard } from './auth.guard';
 
 @NgModule({
   declarations: [AdminComponent, AuthComponent],
@@ -12,6 +13,7 @@ import { FormsModule } from '@angular/forms';
     CommonModule,
     FormsModule,
     AdminRoutingModule
-  ]
+  ],
+  providers:[AuthGuard]
 })
 export class AdminModule { }
