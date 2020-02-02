449db6db55707225209d58300236a45e7eb8f6f7
Akif <akif.dere@rlcteknoloji.com>
2020-02-02 15:34:51 +0300

    Tek dile dönüştürüldü.

--- src/app/shop/navbar/navbar.component.ts
+++ src/app/shop/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/model/auth.service';
 
 @Component({
   selector: 'navbar',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+
   }
 
+  isAuthenticated() {
+    return this.authService.authenticated;
+  }
 }
